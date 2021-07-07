4f48abf09fe1e5cbfed3f6c16f4d6ef4185e4c33
Niluka Sripali <niluka@wso2.com>
2021-07-07 13:05:21 +0530

    update blog test folder

---
+++ cypress/integration/3-blog-examples/test.spec.ts
@@ -0,0 +1,16 @@
+/*
+ *   Copyright (c) 2021 WSO2 Inc. (http://www.wso2.org)
+ *   All rights reserved.
+ *   This software is the property of WSO2 Inc. and its suppliers, 
+ *   if any. Dissemination of any information or reproduction of any material contained 
+ *   herein in any form is strictly forbidden, unless permitted by WSO2 expressly. 
+ *   You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+describe('This is sample test file', () => {
+    
+    it('This is sample Test case 1', () => {
+        
+        cy.log("Sample test log");
+    });
+});
