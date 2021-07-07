2c1e50f9327e52b369a1dbaf637dda9601dec3e2
Niluka Sripali <niluka@wso2.com>
2021-07-07 13:48:37 +0530

    Add Cypress Custom log with winston

---
+++ cypress/integration/3-blog-examples/custom-log-cy-task.spec.ts
@@ -0,0 +1,13 @@
+/*
+ *   Copyright (c) 2021 
+ *   All rights reserved.
+ */
+
+describe(`Demo test to show Cypress task using plugins`, () => {
+    it(`write to log using cy.task`, () => {
+      console.log(`writing to a log file`)
+  
+      cy.task('logInfo', `cy.task info log added`)
+      cy.task('logError', `cy.task error log added`)
+    })
+  })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,7 @@
+/*
+ *   Copyright (c) 2021 
+ *   All rights reserved.
+ */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -16,7 +20,51 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+// winston logger
+const { createLogger, format, transports } = require('winston')
+
+const { combine, timestamp, label, printf } = format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${level.toUpperCase()}] ${message}`
+})
+
+const logger = createLogger({
+  format: combine(label({ label: 'test' }), timestamp(), myFormat),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/info.log' }),
+    new transports.Console({ level: 'error' }),
+  ],
+})
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  on('task', {
+    logInfo(msg) {
+      return new Promise((resolve, reject) => {
+        logger.info(msg)
+        resolve('')
+      })
+    },
+
+    logWarn(msg) {
+      return new Promise((resolve, reject) => {
+        logger.warn(msg)
+        resolve('')
+      })
+    },
+
+    logError(msg) {
+      return new Promise((resolve, reject) => {
+        logger.error(msg)
+        resolve('')
+      })
+    },
+  })
 }
